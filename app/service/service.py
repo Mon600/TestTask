6febf6e549820264f3f9173a52a3aa8a07cbec3d
Mister-Monkster <miki.rulll2012@gmail.com>
2025-08-01 10:44:35 +0300

    fixes

--- app/service/service.py
+++ app/service/service.py
@@ -5,19 +5,20 @@ from app.db.models import OperationType
 from app.db.repository.operation_repository import OperationRepository
 from app.db.repository.history_repositoriy import HistoryRepository
 
-logger = logging.getLogger(__name__)
+
 
 class OperationService:
     def __init__(self, operation_repository: OperationRepository, history_repository: HistoryRepository):
         self.operation_repository = operation_repository
         self.history_repository = history_repository
+        self.logger = logging.getLogger(__name__)
 
     async def create_wallet(self):
         try:
             wallet_id = await self.operation_repository.create()
             return wallet_id
         except Exception as e:
-            logger.error(e)
+            self.logger.error(e)
             return None
 
     async def operation(self, wallet_id: UUID, amount: int, operation: OperationType):
@@ -30,7 +31,7 @@ class OperationService:
                 raise TypeError("Invalid operation type")
             return balance
         except Exception as e:
-            logger.error(e)
+            self.logger.error(e)
             return None
 
     async def balance(self, wallet_id: UUID):
@@ -38,7 +39,7 @@ class OperationService:
             balance = await self.operation_repository.get_balance(wallet_id)
             return balance
         except Exception as e:
-            logger.error(e)
+            self.logger.error(e)
             return None
 
     async def history(self, wallet_id: UUID, limit: int, offset: int):
@@ -46,5 +47,5 @@ class OperationService:
             history = await self.history_repository.get_history(wallet_id, limit, offset)
             return history
         except Exception as e:
-            logger.error(e)
+            self.logger.error(e)
             return None
